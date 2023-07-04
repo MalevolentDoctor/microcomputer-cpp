8a1e0258b1da53780e5855a060832594ee34a6df
Malevolent_Doctor <malevolentdoctor@gmail.com>
2023-07-04 23:02:41 +1000

    implemented delta time

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@ Core *core = nullptr;
 
 int main(int argc, char *argv[]) {
 
-	std::cout << "Hello there" << std::endl;
-
 	core = new Core();
 	core->init("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720);
 
+	Uint64 time_now = SDL_GetPerformanceCounter();
+	Uint64 time_last = 0;
+	double delta_time = 0;
+
 	while (core->running()) {
+
+		time_last = time_now;
+		time_now = SDL_GetPerformanceCounter();
+		delta_time = (double)((time_now - time_last) * 1000 / (double)SDL_GetPerformanceFrequency());
 		
 		core->event();
-		core->update();
+		core->update(delta_time);
 		core->draw();
 	}
 
