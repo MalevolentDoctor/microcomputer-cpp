9fd18a5df2463fd6925a507f09675107ff422748
Malevolent_Doctor <malevolentdoctor@gmail.com>
2023-07-15 23:48:03 +1000

    updated object2d to use sprites

--- src/engine/object2d.cpp
+++ src/engine/object2d.cpp
@@ -1,24 +1,12 @@
 #include "object2d.hpp"
-#include "texturemanager.hpp"
 
-Object2D::Object2D(const char* texture_file, int x_init, int y_init) {
-	obj2d_texture = TextureManager::loadTexture(texture_file);
-	x = x_init;
-	y = y_init;
+Object2D::Object2D(Sprite *sprite) {
+	spr_sprite = sprite;
 }
 
 void Object2D::update(double delta) {
-	src_rect.w = 64;
-	src_rect.h = 64;
-	src_rect.x = 0;
-	src_rect.y = 0;
-
-	dest_rect.x = x;
-	dest_rect.y = y;
-	dest_rect.w = 64;
-	dest_rect.h = 64;
 }
 
 void Object2D::draw() {
-	SDL_RenderCopy(Core::renderer, obj2d_texture, &src_rect, &dest_rect);
+	spr_sprite->draw();
 }
